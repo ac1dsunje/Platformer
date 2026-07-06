7e5da8c7b75bc68c9b62951a4e7446fd2c1b9a40
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-07 00:33:15 +0200

    added spike controller

---
+++ Assets/_Game/Scripts/Level/Spike/SpikeController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SpikeController: MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            Debug.Log("Hit player!");
+        }
+    }
+}
