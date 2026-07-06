7714f21d216816636f88c652a815064a742e3997
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-06 14:40:13 +0200

    added generic abstract state manager class for future fsms

--- Assets/_Game/Scripts/GamePlay/GameStateManager.cs
+++ Assets/_Game/Scripts/GamePlay/GameStateManager.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 
-public class GameStateManager: IDisposable
+public class GameStateManager : StateManager<GameState>, IDisposable
 {
     private readonly PlayerController _player;
     private readonly GeneralInputHandler _input;
-
-    private Dictionary<Type, GameState> _states = new();
-
-    private GameState _state;
 
     public GameStateManager(PlayerController player, GeneralInputHandler input)
     {
@@ -19,38 +14,14 @@ public class GameStateManager: IDisposable
         _player.OnDied += HandlePlayerDied;
     }
 
-    public void AddState(GameState state)
-    {
-        _states.Add(state.GetType(), state);
-    }
-
     private void HandlePlayerDied()
     {
         ChangeState<DeathState>();
     }
 
-    public void ChangeState<T>() where T: GameState
-    {
-        if (IsInState<T>()) return;
-
-        var type = typeof(T);
-
-        if (_states.TryGetValue(type, out var state))
-        {
-            _state?.Exit();
-            _state = state;
-            _state.Enter();
-        }
-    }
-
-    private bool IsInState<T>() where T : GameState
-    {
-        return _state?.GetType() == typeof(T);
-    }
-
     private void RestartGame()
     {
-        if(IsInState<DeathState>())
+        if (IsInState<DeathState>())
             SceneLoader.ReloadGamePlay();
     }
 
