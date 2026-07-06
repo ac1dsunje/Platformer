7403a72415975486764e59e645b32e037a6481ff
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-06 16:59:35 +0200

    added gate controller

---
+++ Assets/_Game/Scripts/Level/Gate/GateController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GateController: MonoBehaviour
+{
+    [SerializeField] private GameObject _openedContainer;
+    [SerializeField] private GameObject _closedContainer;
+
+    public void Open()
+    {
+        _openedContainer.SetActive(true);
+        _closedContainer.SetActive(false);
+    }
+
+    public void Close()
+    {
+        _openedContainer.SetActive(false);
+        _closedContainer.SetActive(true);
+    }
+}
--- Assets/_Game/Scripts/Level/LevelController.cs
+++ Assets/_Game/Scripts/Level/LevelController.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 public class LevelController : MonoBehaviour
 {
     [SerializeField] private Transform _spawnPosition;
-
+    [SerializeField] private GateController _enterGate;
+    [SerializeField] private GateController _exitGate;
     public Transform GetSpawnPosition => _spawnPosition;
 
+    private void Awake()
+    {
+        _enterGate?.Open();
+        _exitGate?.Close();
+    }
 }
