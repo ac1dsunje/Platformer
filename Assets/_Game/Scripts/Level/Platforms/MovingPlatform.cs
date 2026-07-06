e108c379a6e7d8bac4bc6b0a717fcacb7e5d4b27
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-06 21:19:29 +0200

    getting ready for moving platforms and enemies

---
+++ Assets/_Game/Scripts/Level/Platforms/MovingPlatform.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Vector2 _startPoint;
+    [SerializeField] private Vector2 _endPoint;
+    [SerializeField] private float _movementTime;
+    [SerializeField] private float _waitingTime;
+
+
+    [ContextMenu("Set Start Point")]
+    private void SetStartPoint()
+    {
+        _startPoint = new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f);
+    }
+
+    [ContextMenu("Set End Point")]
+    private void SetEndPoint()
+    {
+        _endPoint = new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f);
+    }
+}
