eec0e1c567c090e4672e5ca91f64b8815a11678a
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-07 02:24:04 +0200

    added day/night lighting

--- Assets/_Game/Scripts/Level/LevelController.cs
+++ Assets/_Game/Scripts/Level/LevelController.cs
@@ -3,10 +3,17 @@ using UnityEngine.Rendering.Universal;
 
 public class LevelController : MonoBehaviour
 {
+    [Header("Spawn & Gates")]
     [SerializeField] private Transform _spawnPosition;
     [SerializeField] private GateController _enterGate;
     [SerializeField] private GateController _exitGate;
+
+    [Header("Day/Night Cycle")]
     [SerializeField] private Light2D _light;
+    [SerializeField, Min(1f)] private float _dayDurationSeconds = 120f;
+    [SerializeField] private float _minIntensity = 0.1f;
+    [SerializeField] private float _maxIntensity = 1f;
+
     public Transform GetSpawnPosition => _spawnPosition;
 
     private float _time;
@@ -21,5 +28,13 @@ public class LevelController : MonoBehaviour
     private void Update()
     {
         _time += Time.deltaTime;
+        UpdateLightIntensity();
+    }
+
+    private void UpdateLightIntensity()
+    {
+        float t = (_time % _dayDurationSeconds) / _dayDurationSeconds;
+        float sineValue = (Mathf.Sin(t * Mathf.PI * 2f) + 1f) / 2f;
+        _light.intensity = Mathf.Lerp(_minIntensity, _maxIntensity, sineValue);
     }
 }
